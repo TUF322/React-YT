78455270c79c21a94b6e61684c4bd5467b4de5a0
TUF322 <jdgdg05@gmail.com>
2024-05-03 16:19:31 +0100

    sidebar-3.0

--- src/navbar.js
+++ src/navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${sidebarOpen ? 'sidebar-active' : ''}`}>
       <a className="navbar-brand" href="#">
         <img src={logo} alt="logo" />
       </a>
@@ -31,7 +31,7 @@ const Navbar = () => {
       <div className={`collapse navbar-collapse ${sidebarOpen ? 'show' : ''}`}>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <form className="form-style" >
+            <form className="form-inline my-2 my-lg-0 mx-auto">
               <input
                 className="form-control mr-sm-2"
                 type="search"
@@ -45,11 +45,10 @@ const Navbar = () => {
           </li>
         </ul>
         <ul className="navbar-nav ml-auto">
-            <li className='nav-item'>
+          <li>
             <p className='user-name'>User@gmail.com</p>
-            </li>
-          <li className="nav-item" style={{margin:'14px'}}>
-            
+          </li>
+          <li className="nav-item" style={{paddingLeft:"14px"}}>
             <img src={userIcon} alt="user icon" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
           </li>
         </ul>
@@ -59,26 +58,19 @@ const Navbar = () => {
         {sidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}
       </button>
 
-      {sidebarOpen && (
-        <div className="sidebar active">
-          <div className="sd-header">
-            <h4 className="mb-0">Sidebar Header</h4>
-            <div className="btn btn-primary" onClick={toggleSidebar}><i className="fa fa-times">X</i></div>
-          </div>
-          <div className="sd-body">
-            <ul>
-              <li><a className="sd-link">Menu Item 1</a></li>
-              <li><a className="sd-link">Menu Item 2</a></li>
-              <li><a className="sd-link">Menu Item 3</a></li>
-              <li><a className="sd-link">Menu Item 4</a></li>
-              <li><a className="sd-link">Menu Item 5</a></li>
-              <li><a className="sd-link">Menu Item 6</a></li>
-              <li><a className="sd-link">Menu Item 7</a></li>
-              <li><a className="sd-link">Menu Item 8</a></li>
-            </ul>
-          </div>
-        </div>
-      )}
+      <div className=''>
+      <div className={`sidebar ${sidebarOpen ? 'active' : ''}`}>
+        <h2 className='sd-header'>sidebar Header</h2>
+        <ul>
+          <li className='sd-button'>Menu Item 1</li>
+          <li className='sd-button'>Menu Item 2</li>
+          <li className='sd-button'>Menu Item 3</li>
+        </ul>
+      </div>
+
+      </div>
+
+      
     </nav>
   );
 }
