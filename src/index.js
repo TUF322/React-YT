19be45782d40ad11a75abb68e2f5544fe7145054
TUF322 <jdgdg05@gmail.com>
2024-05-06 10:46:24 +0100

    tags-1.0

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToggleSidebar from './navbar.js'; // Importing the ToggleSidebar component
 import './index.css';
-
+//back color
 document.body.style.backgroundColor = '#202225';
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,10 @@
+// navbar.js
+
 import React, { useState } from 'react';
 import logo from './bg-yt-logo.png';
 import userIcon from './userimg.png';
-import hamburgerBtn from './hamburger-btn.png'; // Import the hamburger button image
+import hamburgerBtn from './hamburger-btn.png';
+import TagsRow from './TagsRow'; // Import the TagsRow component
 import './index.css';
 
 const Navbar = () => {
@@ -12,66 +15,71 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${sidebarOpen ? 'sidebar-active' : ''}`}>
-      <button className="btn btn-outline-light" onClick={toggleSidebar}>
-  {sidebarOpen ? (
-    <img src={hamburgerBtn} alt="close sidebar" style={{ width: '40px', height: '40px' }} />
-  ) : (
-    <img src={hamburgerBtn} alt="open sidebar" style={{ width: '40px', height: '40px' }} />
-  )}
-</button>
+    <div>
+      <nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${sidebarOpen ? 'sidebar-active' : ''}`}>
+        <button className="btn btn-outline-light" onClick={toggleSidebar}>
+          {sidebarOpen ? (
+            <img src={hamburgerBtn} alt="close sidebar" style={{ width: '30px', height: '30px' }} />
+          ) : (
+            <img src={hamburgerBtn} alt="open sidebar" style={{ width: '30px', height: '30px' }} />
+          )}
+        </button>
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleSidebar}
+        >
+          <img src={hamburgerBtn} alt="hamburger button" style={{ width: '30px', height: '30px' }} />
+        </button>
 
-      <a className="navbar-brand" href="#">
-        <img src={logo} alt="logo" />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        onClick={toggleSidebar} // Toggle the sidebar when the button is clicked
-      >
-        <img src={hamburgerBtn} alt="hamburger button" style={{ width: '50px', height: '50px' }} /> {/* Hamburger button image */}
-      </button>
+        <div className={`collapse navbar-collapse ${sidebarOpen ? 'show' : ''}`}>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <form className="form-inline my-2 my-lg-0 mx-auto">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                  Search
+                </button>
+              </form>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li>
+              <p className='user-name'>User@gmail.com</p>
+            </li>
+            <li className="nav-item" style={{ paddingLeft: "14px", paddingTop: "14px" }}>
+              <img src={userIcon} alt="user icon" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+            </li>
+          </ul>
+        </div>
 
-      <div className={`collapse navbar-collapse ${sidebarOpen ? 'show' : ''}`}>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <form className="form-inline my-2 my-lg-0 mx-auto">
-              <input
-                className="form-control mr-sm-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-                Search
-              </button>
-            </form>
-          </li>
-        </ul>
-        <ul className="navbar-nav ml-auto">
-          <li>
-            <p className='user-name'>User@gmail.com</p>
-          </li>
-          <li className="nav-item" style={{ paddingLeft: "14px", paddingTop: "14px" }}>
-            <img src={userIcon} alt="user icon" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
-          </li>
-        </ul>
-      </div>
+        <div>
+          {/* Sidebar Overlay */}
+          <div className={`sidebar-overlay ${sidebarOpen ? 'active' : ''}`} onClick={toggleSidebar}></div>
 
-      {/* Sidebar Overlay */}
-      <div className={`sidebar-overlay ${sidebarOpen ? 'active' : ''}`} onClick={toggleSidebar}></div>
+          {/* Sidebar */}
+          <div className={`sidebar ${sidebarOpen ? 'active' : ''}`}>
+            <h2 className='sd-header'>Menu</h2>
+            <ul>
+              <li className='sd-button'>Menu Item 1</li>
+              <li className='sd-button'>Menu Item 2</li>
+              <li className='sd-button'>Menu Item 3</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-      {/* Sidebar */}
-      <div className={`sidebar ${sidebarOpen ? 'active' : ''}`}>
-        <h2 className='sd-header'>Menu</h2>
-        <ul>
-          <li className='sd-button'>Menu Item 1</li>
-          <li className='sd-button'>Menu Item 2</li>
-          <li className='sd-button'>Menu Item 3</li>
-          <li className='sd-button'>Menu Item 4</li>
-        </ul>
-      </div>
-    </nav>
+      {/* Tags Row */}
+      <TagsRow />
+    </div>
   );
 }
 
